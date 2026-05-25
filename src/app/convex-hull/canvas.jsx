29437cdbc2c45bb237111d89bd38d700df5ea0dc
TamimEhsan <mohammadtamimul.ehsan@optimizely.com>
2026-05-25 15:32:49 +0800

    refactor: Convert Convex Hull components from class to functional with hooks
    Migrates menu, canvas, and page to use useState, useEffect, and useRef.
    Removes unused cnvas2.jsx and timer.jsx.

--- src/app/convex-hull/canvas.jsx
+++ src/app/convex-hull/canvas.jsx
@@ -1,110 +1,80 @@
 import { convex_hull } from "@/lib/algorithms/grahamScan";
-import React, { Component } from 'react';
+import { useState, useEffect, useRef } from 'react';
 
-class Canvas extends Component {
-    state = {
-        dots: [],
-        lines: [],
-        canvasWidth: 300,
-        canvasHeight: 100
-    }
-    constructor(props) {
-        super(props);
-        this.myRef = React.createRef();
-        this.canvasLineRef = React.createRef();
-    }
-    componentDidMount() {
-        this.redrawDots();
-        this.setState({ canvasWidth: this.props.width });
-        this.setState({ canvasHeight: this.props.height });
-        // console.log(this.state.canvasWidth);
-    }
-    componentDidUpdate(prevProps, prevState, snapshot) {
+export default function Canvas({ width, height, dots, onTurnOff, onGoing, speed }) {
+    const [lines, setLines] = useState([]);
+    const dotsCanvasRef = useRef(null);
+    const linesCanvasRef = useRef(null);
+    const onGoingRef = useRef(onGoing);
+    const speedRef = useRef(speed);
 
-        if (this.props.dots !== prevProps.dots) {
-            this.setState({ dots: this.props.dots });
-            this.redrawDots();
+    useEffect(() => { onGoingRef.current = onGoing; }, [onGoing]);
+    useEffect(() => { speedRef.current = speed; }, [speed]);
+
+    useEffect(() => {
+        redrawDots();
+    }, [dots]);
+
+    useEffect(() => {
+        if (onGoing) {
+            animateLine();
         }
-        if (this.props.onGoing !== prevProps.onGoing) {
-            if (this.props.onGoing === true) {
-                this.animateLine();
-            }
-        }
-    }
+    }, [onGoing]);
 
-    render() {
-        return (
-            <>
-                <canvas
-                    className='absolute'
-                    id='canvas1'
-                    // style={{backgroundColor:"whitesmoke"}}
-                    ref={this.canvasLineRef} width={this.props.width} height={this.props.height}
-                />
-                <canvas
-                    className='absolute'
-                    id='canvas2'
-                    // style={{backgroundColor:"grey"}}
-                    ref={this.myRef} width={this.props.width} height={this.props.height}
-                />
-
-            </>
-        );
-    }
-
-    redrawDots() {
-        const canvas = this.myRef.current;
+    const redrawDots = () => {
+        const canvas = dotsCanvasRef.current;
+        if (!canvas) return;
         const ctx = canvas.getContext("2d");
         ctx.clearRect(0, 0, canvas.width, canvas.height);
 
         ctx.fillStyle = 'lightgrey';
         ctx.strokeStyle = 'black';
-        for (let i = 0; i < this.props.dots.length; i++) {
+        for (let i = 0; i < dots.length; i++) {
             ctx.beginPath();
-            // ctx.moveTo(this.props.dots[i].xx, this.props.dots[i].yy)
-            ctx.arc(this.props.dots[i].xx, this.props.dots[i].yy, 10, 0, 2 * Math.PI);
+            ctx.arc(dots[i].xx, dots[i].yy, 10, 0, 2 * Math.PI);
             ctx.fill();
             ctx.lineWidth = 2;
             ctx.stroke();
             ctx.closePath();
-
         }
         ctx.closePath();
-        const res = convex_hull(this.props.dots);
 
-        const cansvas2 = this.canvasLineRef.current;
-        const ctx2 = cansvas2.getContext('2d');
-        ctx2.clearRect(0, 0, canvas.width, canvas.height);
-        this.setState({ lines: res[1] });
+        const res = convex_hull(dots);
+        const canvas2 = linesCanvasRef.current;
+        if (!canvas2) return;
+        const ctx2 = canvas2.getContext('2d');
+        ctx2.clearRect(0, 0, canvas2.width, canvas2.height);
+        setLines(res[1]);
+    };
 
-    }
-
-    animateLine = async () => {
-        const { lines } = this.state;
-        const canvas2 = this.canvasLineRef.current;
+    const animateLine = async () => {
+        const currentLines = lines;
+        const canvas2 = linesCanvasRef.current;
+        if (!canvas2) return;
         const ctx2 = canvas2.getContext('2d');
         ctx2.clearRect(0, 0, canvas2.width, canvas2.height);
         ctx2.fillStyle = '#ffffff';
         ctx2.strokeStyle = '#ffffff';
-        for (let i = 0; i < lines.length; i++) {
-            if (!this.props.onGoing) {
+
+        for (let i = 0; i < currentLines.length; i++) {
+            if (!onGoingRef.current) {
                 ctx2.clearRect(0, 0, canvas2.width, canvas2.height);
                 return;
             }
             ctx2.beginPath();
-            if (lines[i].add) {
+            if (currentLines[i].add) {
                 ctx2.beginPath();
                 ctx2.lineWidth = 2;
                 ctx2.fillStyle = 'black';
-                ctx2.moveTo(lines[i].from.xx, lines[i].from.yy)
-                ctx2.arc(lines[i].from.xx, lines[i].from.yy, 14, 0, 2 * Math.PI);
+                ctx2.moveTo(currentLines[i].from.xx, currentLines[i].from.yy);
+                ctx2.arc(currentLines[i].from.xx, currentLines[i].from.yy, 14, 0, 2 * Math.PI);
                 ctx2.fill();
                 ctx2.closePath();
 
                 ctx2.beginPath();
                 ctx2.fillStyle = 'red';
-                ctx2.moveTo(lines[i].to.xx, lines[i].to.yy)
-                ctx2.arc(lines[i].to.xx, lines[i].to.yy, 14, 0, 2 * Math.PI);
+                ctx2.moveTo(currentLines[i].to.xx, currentLines[i].to.yy);
+                ctx2.arc(currentLines[i].to.xx, currentLines[i].to.yy, 14, 0, 2 * Math.PI);
                 ctx2.fill();
                 ctx2.closePath();
 
@@ -114,15 +84,15 @@ class Canvas extends Component {
             } else {
                 ctx2.beginPath();
                 ctx2.fillStyle = 'whitesmoke';
-                ctx2.moveTo(lines[i].from.xx, lines[i].from.yy)
-                ctx2.arc(lines[i].from.xx, lines[i].from.yy, 15, 0, 2 * Math.PI);
+                ctx2.moveTo(currentLines[i].from.xx, currentLines[i].from.yy);
+                ctx2.arc(currentLines[i].from.xx, currentLines[i].from.yy, 15, 0, 2 * Math.PI);
                 ctx2.fill();
                 ctx2.closePath();
 
                 ctx2.beginPath();
                 ctx2.fillStyle = 'whitesmoke';
-                ctx2.moveTo(lines[i].to.xx, lines[i].to.yy)
-                ctx2.arc(lines[i].to.xx, lines[i].to.yy, 15, 0, 2 * Math.PI);
+                ctx2.moveTo(currentLines[i].to.xx, currentLines[i].to.yy);
+                ctx2.arc(currentLines[i].to.xx, currentLines[i].to.yy, 15, 0, 2 * Math.PI);
                 ctx2.fill();
                 ctx2.closePath();
 
@@ -130,19 +100,35 @@ class Canvas extends Component {
                 ctx2.strokeStyle = 'whitesmoke';
             }
 
-            ctx2.moveTo(lines[i].from.xx, lines[i].from.yy);
-            ctx2.lineTo(lines[i].to.xx, lines[i].to.yy);
+            ctx2.moveTo(currentLines[i].from.xx, currentLines[i].from.yy);
+            ctx2.lineTo(currentLines[i].to.xx, currentLines[i].to.yy);
             ctx2.stroke();
             ctx2.closePath();
-            if (i === lines.length - 1) {
-                this.props.onTurnOff();
+            if (i === currentLines.length - 1) {
+                onTurnOff();
             }
-            await sleep(this.props.speed);
+            await sleep(speedRef.current);
         }
-    }
+    };
 
+    return (
+        <>
+            <canvas
+                className="absolute"
+                ref={linesCanvasRef}
+                width={width}
+                height={height}
+            />
+            <canvas
+                className="absolute"
+                ref={dotsCanvasRef}
+                width={width}
+                height={height}
+            />
+        </>
+    );
 }
+
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
-export default Canvas;
--- src/app/convex-hull/page.jsx
+++ src/app/convex-hull/page.jsx
@@ -1,114 +1,94 @@
 "use client";
-import { Component, createRef } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import Navbar from '@/components/navbar';
 import Canvas from "./canvas";
 import Menu from "./menu";
-class ConvexHull extends Component {
-    
-    constructor() {
-        super();
-        this.state = {
-            dots: [],
-            lines: [],
-            isALgoLive: false,
-            width: 100,
-            height: 100,
-            isRunning: false,
-            speed: 100,
-            number: 50
-        }
-        this.containerRef = createRef();
-    }
-    componentDidMount() {
-        this.setState({ 
-            width: this.containerRef.current.offsetWidth, 
-            height: this.containerRef.current.offsetHeight 
-        });
-        console.log(this.containerRef.current.offsetWidth);
-    }
-    render() {
-        return (
-            <div className="flex flex-col h-screen">
-                <Navbar title="Convex Hull" />
-                <div className="flex flex-1 overflow-hidden">
-                    <Menu
-                        onRefresh={this.handleRefreshDots}
-                        onVisualize={this.handleVisualize}
-                        onChangeSpeed={this.changeSpeed}
-                        onChangeValues={this.handleValueIncease}
-                        isDisabled={this.state.isRunning}
-                    />
-                    <div className="flex flex-1 flex-col items-center justify-center overflow-auto">
-                        <div className="w-full h-full flex items-center justify-center" ref={this.containerRef}>
-                            <Canvas
-                                width={this.state.width}
-                                height={this.state.height}
-                                dots={this.state.dots}
-                                onTurnOff={this.handleTurnOff}
-                                onGoing={this.state.isRunning}
-                                speed={this.state.speed}
-                            />
-                        </div>
+
+export default function ConvexHull() {
+    const [dots, setDots] = useState([]);
+    const [isRunning, setIsRunning] = useState(false);
+    const [width, setWidth] = useState(100);
+    const [height, setHeight] = useState(100);
+    const [speed, setSpeed] = useState(100);
+    const [number, setNumber] = useState(50);
+
+    const containerRef = useRef(null);
+    const numberRef = useRef(number);
+
+    useEffect(() => { numberRef.current = number; }, [number]);
+
+    useEffect(() => {
+        setWidth(containerRef.current.offsetWidth);
+        setHeight(containerRef.current.offsetHeight);
+    }, []);
+
+    const handleValueIncrease = (value) => {
+        setNumber(value);
+        setIsRunning(false);
+        setDots(getNewDots(value, width, height));
+    };
+
+    const changeSpeed = (speed) => {
+        setSpeed(600 - speed * 10);
+    };
+
+    const handleVisualize = () => {
+        setIsRunning(true);
+    };
+
+    const handleTurnOff = () => {
+        setIsRunning(false);
+    };
+
+    const handleRefreshDots = () => {
+        setIsRunning(false);
+        setDots(getNewDots(numberRef.current, width, height));
+    };
+
+    return (
+        <div className="flex flex-col h-screen">
+            <Navbar />
+            <div className="flex flex-1 overflow-hidden">
+                <Menu
+                    onRefresh={handleRefreshDots}
+                    onVisualize={handleVisualize}
+                    onChangeSpeed={changeSpeed}
+                    onChangeValues={handleValueIncrease}
+                    disabled={isRunning}
+                />
+                <div className="flex flex-1 flex-col items-center justify-center overflow-auto">
+                    <div className="w-full h-full flex items-center justify-center" ref={containerRef}>
+                        <Canvas
+                            width={width}
+                            height={height}
+                            dots={dots}
+                            onTurnOff={handleTurnOff}
+                            onGoing={isRunning}
+                            speed={speed}
+                        />
                     </div>
                 </div>
             </div>
-        );
-    }
-    handleValueIncease = (value) => {
-        this.setState({ number: value, isRunning: false, dots: getNewDots(value, this.state.width, this.state.height) });
-    }
-    changeSpeed = (speed) => {
-        //console.log(typeof speed);
-        this.setState({ speed: 600 - speed * 10 });
-    }
-    handleAlgoStateChanged = (val) => {
-        this.setState({ isAlgoLive: val });
-    }
-    handleVisualize = () => {
-        this.setState({ isRunning: true });
-    }
-    handleTurnOff = () => {
-        this.setState({ isRunning: false });
-    }
-    handleRefreshDots = () => {
-        this.setState({ isRunning: false });
-        this.setState({ dots: getNewDots(this.state.number, this.state.width, this.state.height) });
-    }
-
-    handleMoreDot = () => {
-        const row = Math.floor(Math.random() * 400) + 10;
-        const col = Math.floor(Math.random() * 400) + 10;
-        const dot = {
-            row: row,
-            col: col
-        }
-        const dots = this.state.dots;
-        dots.push(dot);
-        this.setState(dots);
-    }
+        </div>
+    );
 }
+
 function getNewDots(number, width, height) {
     const dots = [];
     for (let i = 0; i < number; i++) {
         dots.push(getDot(width, height));
     }
     dots.sort((a, b) => {
-        if (a.xx !== b.xx) {
-            return a.xx - b.xx;
-        } else {
-            return a.yy - b.yy;
-        }
+        if (a.xx !== b.xx) return a.xx - b.xx;
+        return a.yy - b.yy;
     });
     return dots;
 }
+
 function getDot(width, height) {
     width = width - 50;
     height = height - 50;
     const rowpos = Math.floor(Math.random() * height) + 25;
     const colpos = Math.floor(Math.random() * width) + 25;
-    return {
-        xx: colpos,
-        yy: rowpos,
-    }
+    return { xx: colpos, yy: rowpos };
 }
-export default ConvexHull;
