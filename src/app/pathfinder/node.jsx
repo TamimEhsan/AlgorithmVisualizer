b01f868ba5951b6bae5b43191dfb4bebbe479487
TamimEhsan <mohammadtamimul.ehsan@optimizely.com>
2026-05-25 15:36:02 +0800

    refactor: Convert Pathfinder components from class to functional with hooks
    Migrates node, grid, menu, and page components. Uses useRef for isRunning
    guard in async animations. Removes unused state and commented-out code.

--- src/app/pathfinder/node.jsx
+++ src/app/pathfinder/node.jsx
@@ -1,37 +1,22 @@
-import React, {Component} from 'react';
-import './node.css'
-class Node extends Component {
-    static = {
-        row: this.props.row,
-        col: this.props.col
-    }
-    render() {
-        const { row,col,isVisited, onMouseDown, onMouseEnter,onMouseUp } = this.props;
-        return (
-            <div
-                id = {`node-${this.props.row}-${this.props.col}`}
-                className={this.getClassName()}
-                onMouseDown={() => onMouseDown(row,col)}
-                onMouseEnter={() => onMouseEnter(row,col)}
-                onMouseUp={() => onMouseUp(row,col)}
-            />
-        );
-    }
-    getClassName(){
-        if(this.props.node.isWall === true){
-            return "node node-wall";
-        } else if( this.props.node.isStartNode === true ){
-            return "node  node-start";
-        } else if( this.props.node.isEndNode === true ){
-            return "node  node-end";
-        } else if(this.props.node.ispathNode){
-            return 'node node-shortest-path';
-        }else if( this.props.node.isVisited === true ){
-            return "node  node-visited";
-        } else{
-            return "node";
-        }
-    }
-}
+import './node.css';
 
-export default Node;
+export default function Node({ row, col, node, onMouseDown, onMouseEnter, onMouseUp }) {
+    const getClassName = () => {
+        if (node.isWall) return "node node-wall";
+        if (node.isStartNode) return "node node-start";
+        if (node.isEndNode) return "node node-end";
+        if (node.ispathNode) return "node node-shortest-path";
+        if (node.isVisited) return "node node-visited";
+        return "node";
+    };
+
+    return (
+        <div
+            id={`node-${row}-${col}`}
+            className={getClassName()}
+            onMouseDown={() => onMouseDown(row, col)}
+            onMouseEnter={() => onMouseEnter(row, col)}
+            onMouseUp={() => onMouseUp(row, col)}
+        />
+    );
+}
